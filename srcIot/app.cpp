6d19d4ed352cf1334b6cb3c5766ae93e9ac0ff32
Enzo Fatobene <enzoamafatobene@gmail.com>
2025-09-10 08:52:39 -0300

    Configuração do Potenciometro

--- srcIot/app.cpp
+++ srcIot/app.cpp
@@ -3,6 +3,9 @@
 
 Adafruit_MPU6050 mpuSensor; // Declaração do Sensor MPU
 
+#define PotPin 15
+
+int bpm = 0 // inicialização da varável de bpm
 
 void setup() {
   Serial.begin(115200); // Inicalização do Serial, para fins de teste
@@ -19,10 +22,13 @@ void setup() {
   mpu.setGyroRange(MPU6050_RANGE_500_DEG); // Declarando o raio de rotação do Sensor
   mpu.setFilterBandwidth(MPU6050_BAND_5_HZ); // Declarando o raio de banda do Sensor
 
+  pinMode(PotPin, INPUT);
+
 }
 
 void loop() {
   delay(10);
+  int valorPot = analogRead(PotPin);
   eventos_sensor = a, r, temp; // Declarando todos os eventos coletados pelo sensor Aceleração (a), Rotação (r), Temperatura (temp)
   mpuSensor.getEvent(&a, &r, &temp); // Lendo os eventos captados pelo sensor
   Serial.println("Dados da aceleração"); // imprimindo dados da aceleração de cada eixo
@@ -33,4 +39,7 @@ void loop() {
   // imprimindo dados da temperatura local
   Serial.println("Dados da Temperatura");
   Serial.println("Temperatura do ambiente: " + string(temp.acceleration.x) + "°");
+
+  bpm = map(valorPot, 0, 4095, 60, 200) // Mapeando valor do potenciômetro para simular um batimento cardíaco real
+  Serial.println("Bpm: " + string(bpm)); // Imprime o valor simulado
 }
