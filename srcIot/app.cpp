f1d97f4ff2e426956c6d54484008680fd12b8c9b
Enzo Fatobene <enzoamafatobene@gmail.com>
2025-09-10 09:14:57 -0300

    Depuração de erros

--- srcIot/app.cpp
+++ srcIot/app.cpp
@@ -5,12 +5,12 @@ Adafruit_MPU6050 mpuSensor; // Declaração do Sensor MPU
 
 #define PotPin 15
 
-int bpm = 0 // inicialização da varável de bpm
+int bpm = 0; // inicialização da varável de bpm
 
 void setup() {
   Serial.begin(115200); // Inicalização do Serial, para fins de teste
 
-  if (!mpu.begin()) { // Caso falhe a inialização do Sensor MPU
+  if (!mpuSensor.begin()) { // Caso falhe a inialização do Sensor MPU
     Serial.println("Falha ao inicializar Sensor MPU");
     while (1) {
       delay(10);
@@ -19,8 +19,8 @@ void setup() {
   Serial.println("MPU encontrado");
 
   mpuSensor.setAccelerometerRange(MPU6050_RANGE_8_G); // Declarando o raio de acelaração do Sensor
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG); // Declarando o raio de rotação do Sensor
-  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ); // Declarando o raio de banda do Sensor
+  mpuSensor.setGyroRange(MPU6050_RANGE_500_DEG); // Declarando o raio de rotação do Sensor
+  mpuSensor.setFilterBandwidth(MPU6050_BAND_5_HZ); // Declarando o raio de banda do Sensor
 
   pinMode(PotPin, INPUT);
 
@@ -29,17 +29,29 @@ void setup() {
 void loop() {
   delay(10);
   int valorPot = analogRead(PotPin);
-  eventos_sensor = a, r, temp; // Declarando todos os eventos coletados pelo sensor Aceleração (a), Rotação (r), Temperatura (temp)
+  sensors_event_t a, r, temp; // Declarando todos os eventos coletados pelo sensor Aceleração (a), Rotação (r), Temperatura (temp)
   mpuSensor.getEvent(&a, &r, &temp); // Lendo os eventos captados pelo sensor
   Serial.println("Dados da aceleração"); // imprimindo dados da aceleração de cada eixo
-  Serial.println("Aceleração eixo X: " + string(a.acceleration.x) + "m/s²");
-  Serial.println("Aceleração eixo Y: " + string(a.acceleration.y) + "m/s²");
-  Serial.println("Aceleração eixo Z: " + string(a.acceleration.z) + "m/s²");
+  Serial.print("Aceleração eixo X: ");
+  Serial.print(a.acceleration.x);
+  Serial.println(" m/s²");
+  Serial.print("Aceleração eixo Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.println(" m/s²");
+  Serial.print("Aceleração eixo Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s²");
+  
 
   // imprimindo dados da temperatura local
   Serial.println("Dados da Temperatura");
-  Serial.println("Temperatura do ambiente: " + string(temp.acceleration.x) + "°");
+  Serial.print("Temperatura ambiente: ");
+  Serial.print(temp.temperature);
+  Serial.println("°");
 
-  bpm = map(valorPot, 0, 4095, 60, 200) // Mapeando valor do potenciômetro para simular um batimento cardíaco real
-  Serial.println("Bpm: " + string(bpm)); // Imprime o valor simulado
+  bpm = map(valorPot, 0, 4095, 60, 200); // Mapeando valor do potenciômetro para simular um batimento cardíaco real
+  Serial.print("Bpm: "); // Imprime o valor simulado
+  Serial.println(bpm);
+
+  delay(5000); // Intervalo do envio de dados
 }
