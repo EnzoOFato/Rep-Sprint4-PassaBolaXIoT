e9b96f49a4814ac3fec616025a99764801a6a88f
Enzo Fatobene <enzoamafatobene@gmail.com>
2025-09-11 01:10:17 -0300

    Conexão Wifi e Brooker

--- srcIot/app.cpp
+++ srcIot/app.cpp
@@ -1,16 +1,59 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
+#include <Wifi.h>
+#include <PubSubClient.h>
 
 Adafruit_MPU6050 mpuSensor; // Declaração do Sensor MPU
 
 #define PotPin 15
 
+
+// Configuração da Rede
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+
+// Configuração HiveMQ
+const char* mqtt_server = "8ba3b8e02cee4063850876e30806f07c.s1.eu.hivemq.cloud";  // endereço do cluster
+const int mqtt_port = 8883;  // Porta segura
+const char* mqtt_user = "hivemq.webclient.1757562136665";  
+const char* mqtt_password = "l.8I!:P3Ve<qWhu4Jv7R"; 
+const char* mqtt_topic = "iot/sensores/desempenho";  // tópico central
+
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+
 int bpm = 0; // inicialização da varável de bpm
+
+void setup_wifi() { //Método de conexão wifi via próprio provedor Wokwi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) { // Enquanto não estiver conectado ao wifi
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi conectado");
+}
+
+void reconectar() { // Reconectar ao HiveMQ
+  while (!client.connected()) {
+    Serial.print("Tentando conectar ao HiveMQ...");
+    if (client.connect("ESP32Client", mqtt_user, mqtt_password)) {
+      Serial.println("Conectado ao HiveMQ Cluster!"); // Sucesso na Reconexão
+    } else {
+      Serial.print("Falha, rc= ");
+      Serial.println(client.state()); // Mensagem da falha
+      delay(2000);
+    }
+  }
+}
 
 void setup() {
   Serial.begin(115200); // Inicalização do Serial, para fins de teste
 
-  if (!mpuSensor.begin()) { // Caso falhe a inialização do Sensor MPU
+  setup_wifi(); //Conectar ao Wifi
+  espClient.setInsecure(); // Para testes sem certificado CA
+  client.setServer(mqtt_server, mqtt_port); // Conexão do Setup ao HiveMQ
+
+  if (!mpuSensor.begin()) { // Caso falhe a inialização do Sensor MPU (Isso em um cenário real, não falhará no Wokwi)
     Serial.println("Falha ao inicializar Sensor MPU");
     while (1) {
       delay(10);
@@ -27,6 +70,13 @@ void setup() {
 }
 
 void loop() {
+
+  if(!client.connected()){ // Caso a conexão caía durante o andamento da aplicação
+    reconectar();
+  }
+
+  client.loop();
+
   delay(10);
   int valorPot = analogRead(PotPin);
   sensors_event_t a, r, temp; // Declarando todos os eventos coletados pelo sensor Aceleração (a), Rotação (r), Temperatura (temp)
@@ -53,5 +103,16 @@ void loop() {
   Serial.print("Bpm: "); // Imprime o valor simulado
   Serial.println(bpm);
 
-  delay(5000); // Intervalo do envio de dados
+  String envio = "{";
+  envio += "\"bpm\": " + String(bpm) + ",";
+  envio += "\"temp\": " + String(temp.temperature) + ",";
+  envio += "\"ax\": " + String(a.acceleration.x) + ",";
+  envio += "\"ay\": " + String(a.acceleration.y) + ",";
+  envio += "\"az\": " + String(a.acceleration.z);
+  envio += "}"; // Construindo a mensagem a ser enviada ao HiveMQ
+
+  client.publish(mqtt_topic, envio.c_str()); // Publicando a mensagem no Brooker
+  Serial.println("Dados enviados: " + envio);
+
+  delay(2000); // Tempo para a próxima mensagem
 }
